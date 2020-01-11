502fb675cc074761a9aea6eb5834e392a226e118
Jace Benson <jace.benson@protonmail.com>
2020-01-10 21:45:59 -0600

    added touch event

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,12 @@
-
-jQuery('#translate').on('click', function () {
-    var text = jQuery('#text').val();
-    console.log('translating', text);
+function translate(text){
     var endpoint = "/emoji?text=" + text;
     jQuery.get(endpoint, function (data) {
-        console.log(data);
         jQuery('#result').text(data.text);
     });
+}
+jQuery('#translate').on('touchstart', function(){
+    translate(jQuery('#text').val());
+});
+jQuery('#translate').on('click', function () {
+    translate(jQuery('#text').val());
 });
