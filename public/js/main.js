5246ede1213db83ba1179de913f971c89fbfdc21
Jace Benson <jace.benson@protonmail.com>
2020-01-10 22:49:23 -0600

    added api stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,19 @@
 function translate(text){
-    var endpoint = "/emoji?text=" + text;
-    jQuery('#result').text("loading...");
+    var endpoint = '/emoji?text=' + text;
+    jQuery('#result').text('loading...');
     jQuery('#text').val(text);
+    var apiEndpoint = window.location.protocol + '//';
+    apiEndpoint += window.location.host + endpoint;
+    jQuery('#api-request').text(apiEndpoint);
+    jQuery('#api-request').attr('href',apiEndpoint);
     jQuery.get(endpoint, function (data) {
+        jQuery('#api-result').text(JSON.stringify(data));
         jQuery('#result').text(data.text);
     });
 }
 
-
 var urlParams = new URLSearchParams(window.location.search);
 if(urlParams.has('text')){
     translate(urlParams.get('text'));
-} // true
+}
 
