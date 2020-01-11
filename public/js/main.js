ad608fad551597400d56db1001216307d4ee7e15
Jace Benson <jace.benson@protonmail.com>
2020-01-10 22:29:53 -0600

    adding submit

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,15 @@
 function translate(text){
     var endpoint = "/emoji?text=" + text;
     jQuery('#result').text("loading...");
+    jQuery('#text').val(text);
     jQuery.get(endpoint, function (data) {
         jQuery('#result').text(data.text);
     });
 }
-var lastValue = "";
-setInterval(function(){
-    var newValue = jQuery('#text').val();
-    if(lastValue != newValue){
-        lastValue = newValue;
-        translate(newValue);
-    }
-}, 1000);
+
+
+var urlParams = new URLSearchParams(window.location.search);
+if(urlParams.has('text')){
+    translate(urlParams.get('text'));
+} // true
+
